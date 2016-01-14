6b995c9c3f38cdb1a7a7d0de56940738206486ae
David <scaifes@gmail.com>
2016-01-13 21:03:08 -0800

    challenge

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var firstname = prompt("What is your first name?");
+var lastname = prompt("What is your last name?");
+console.log(firstname);
+alert(lastname);
+var userChoice = ("do you really want to do that?");
+alert(userChoice);
